c0061424bee5193565e632f0528a9157379fb15f
GyoJoong <david419kr@gmail.com>
2020-12-30 04:27:57 +0900

    30-뉴스를 스크랩하여 DB에 저장하기 / 스크랩 업로드를 위한 엔드포인트와 컨트롤러를 새로 만들고, 시퀄라이즈를 이용해 DB에 뉴스와 스크랩이 저장되도록 하였다.

--- routes/newsSearch.js
+++ routes/newsSearch.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const { newsSearchController } = require('../controllers');
 
 router.post('/bing',newsSearchController.bingSearch) //bing search(메인화면)
+router.post('/scrap/upload',newsSearchController.dbUpload) //bing 검색 결과를 스크랩하여 db에 업로드
 router.post('/scrap',newsSearchController.dbSearch) //db 내에서 스크랩 찾기
 router.patch('/scrap',newsSearchController.dbModify) //db 내에서 스크랩 수정
 router.delete('/scrap',newsSearchController.dbDelete) //db 내에서 스크랩 삭제
