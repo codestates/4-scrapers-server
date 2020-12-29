368ec5ed7aa21ad6edd07c832a040cd834b2c28c
somniumry <somniumry>
2020-12-29 17:00:36 +0900

    14-회원탈퇴 구현 / accessToken을 verify한 이메일을 이용하여 비밀번호 검증 및 jwt와 .env.accessToken 추가

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -6,22 +6,18 @@ const ACCESS_SECRET = process.env.ACCESS_SECRET;
 module.exports = async (req, res) => {
 
     if (!req.headers.authorization) res.status(400).send({ data: null, message: 'invalid access token' });
-
     else {
         const authorization = req.headers.authorization;
         const token = authorization.split(' ')[1];
         try {
             const data = jwt.verify(token, ACCESS_SECRET);
             let password;
-
             // 비밀번호 검증
             try {
                 password = await bcrypt.compare(req.body.password, (await user.findOne({ where: { email: data.email } })).dataValues.password)
             } catch {
                 password = null;
             }
-
-
             if (password) {
                 await user.destroy({ where: { email: req.body.email } });
                 res.status(200).send({ data: null, message: 'deleteUser complete' })
@@ -33,6 +29,4 @@ module.exports = async (req, res) => {
             res.status(400).send({ data: err, message: 'invalid access token' });
         }
     }
-
 }
-
