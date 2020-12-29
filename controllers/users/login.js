d255753a4e92e099cb3dab084ce44bbed7cf95b6
GyoJoong <david419kr@gmail.com>
2020-12-29 11:29:38 +0900

    11-로그인 구현 / 회원가입시의 bcrypt 사용에 맞춰 로그인 로직 수정

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,12 +1,17 @@
 const { user } = require('../../models');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const ACCESS_SECRET = process.env.ACCESS_SECRET;
 const REFRESH_SECRET = process.env.REFRESH_SECRET;
 
 module.exports = async (req, res) => {
-    let userInfo = await user.findOne({
-        where: { email: req.body.email, password: req.body.password }
-    })
+    const password = await bcrypt.compare(req.body.password, (await user.findOne({ where: { email: req.body.email } })).dataValues.password)
+    let userInfo = null;
+    if (password) {
+        userInfo = await user.findOne({
+            where: { email: req.body.email }
+        })
+    }
 
     if (!userInfo) {
         res.status(400).send({
@@ -26,11 +31,11 @@ module.exports = async (req, res) => {
         }, REFRESH_SECRET, {
             expiresIn: "7 days"
         })
-        await user.update({accessToken, refreshToken}, { where: { email: req.body.email } });
+        await user.update({ accessToken, refreshToken }, { where: { email: req.body.email } });
         res.status(200)
             .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'none' })
             .send({
-                data: {id: userInfo.dataValues.id, email: userInfo.dataValues.email},
+                data: { id: userInfo.dataValues.id, email: userInfo.dataValues.email },
                 accessToken,
                 message: 'login ok'
             })
