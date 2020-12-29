95e59b322de8f568cfe8cc55c07e9e76f7f3436d
somniumry <somniumry>
2020-12-29 15:41:22 +0900

    14-회원탈퇴 구현/accessToken을 verify한 이메일을 이용하여 비밀번호를 검증 및 bcrypt.compare을 try~catch로 수정함

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -3,15 +3,34 @@ const bcrypt = require('bcrypt');
 
 module.exports = async (req, res) => {
 
-    // 비밀번호를 검증
-    const password = await bcrypt.compare(req.body.password, (await user.findOne({ where: {email: req.body.email}})).dataValues.password);
-    
-    if(password) {
-        await user.destroy({where: {email: req.body.email}});
-        res.status(200).send({ data: null, message: 'deleteUser complete'})
-    }
+    if (!req.headers.authorization) res.status(400).send({ data: null, message: 'invalid access token' });
+
     else {
-        res.status(400).send({ data: null, message: 'unComplete deleteUser'})
+        const authorization = req.headers.authorization;
+        const token = authorization.split(' ')[1];
+        try {
+            const data = jwt.verify(token, ACCESS_SECRET);
+            let password;
+
+            // 비밀번호 검증
+            try {
+                password = await bcrypt.compare(req.body.password, (await user.findOne({ where: { email: data.email } })).dataValues.password)
+            } catch {
+                password = null;
+            }
+
+
+            if (password) {
+                await user.destroy({ where: { email: req.body.email } });
+                res.status(200).send({ data: null, message: 'deleteUser complete' })
+            }
+            else {
+                res.status(400).send({ data: null, message: 'unComplete deleteUser' })
+            }
+        } catch (err) {
+            res.status(400).send({ data: err, message: 'invalid access token' });
+        }
     }
-    
+
 }
+
