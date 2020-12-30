704415c4eab7d6e3103ca079157cd0f667567116
somniumry <somniumry>
2020-12-30 11:30:38 +0900

    16-유저 정보 수정/닉네임 수정 시 발생하는 에러 해결

--- controllers/users/patchUser.js
+++ controllers/users/patchUser.js
@@ -1,17 +1,43 @@
 const { user } = require('../../models');
+const jwt = require('jsonwebtoken');
+const ACCESS_SECRET = process.env.ACCESS_SECRET;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = async (req, res) => {
-    console.log(req.body);
-    if (req.body.password && req.body.name) {
-        await user.update({password: req.body.password, name: req.body.name}, { where: { email: 'test' } });
-        res.status(200).send({ data: null, message: 'ok' });
-    } else if (req.body.password) {
-        await user.update({password: req.body.password}, { where: { email: 'test' } });
-        res.status(200).send({ data: null, message: 'ok' });
-    } else if (req.body.name) {
-        await user.update({name: req.body.name}, { where: { email: 'test' } });
-        res.status(200).send({ data: null, message: 'ok' });
-    } else {
-        res.status(400).send({ data: null, message: 'empty request' });
+    const { name, password } = req.body;
+
+    // accessToken 유효성 검사
+    if (!req.headers.authorization) {
+        res.status(400).send({ data: null, message: 'invalid access Token' })
     }
+    else {
+        const token = req.headers.authorization.split(' ')[1];
+        
+        try {
+            const verifyToken = jwt.verify(token, ACCESS_SECRET);
+            
+            if (name && password) {
+                const hash = await bcrypt.hash(req.body.password, saltRounds);
+                await user.update({ name, password: hash }, { where: { email: verifyToken.email } })
+                res.status(200).send({ data: null, message: 'nickName And password Update.' })
+            }
+            else if (name) {
+                await user.update({ name }, { where: { email: verifyToken.email } })
+                res.status(200).send({ data: null, message: 'nickName Update.' })
+            }
+            else if (password) {
+                const hash = await bcrypt.hash(req.body.password, saltRounds);
+                await user.update({ password: hash }, { where: { email: verifyToken.email } })
+                res.status(200).send({ data: null, message: 'password Update.' })
+            }
+            else {
+                res.status(400).send({ data: null, message: 'empty Request. Please fill in the blanks.' })
+            }
+        }
+        catch {
+            res.status(400).send({ data: null, message: 'invalid access Token' })
+        }
+    }
+
 }
