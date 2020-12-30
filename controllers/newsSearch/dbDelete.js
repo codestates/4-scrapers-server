5d790e812b2a3dd818eedac079f969024c5d64d4
GyoJoong <david419kr@gmail.com>
2020-12-30 11:02:43 +0900

    21-스크랩 수정이나 삭제 요청 받을 시 그에 맞춰 DB 수정 / 수정과 삭제 구현

--- controllers/newsSearch/dbDelete.js
+++ controllers/newsSearch/dbDelete.js
@@ -1,5 +1,23 @@
-const { user } = require('../../models');
+const { scrap } = require('../../models');
+const jwt = require('jsonwebtoken');
+const ACCESS_SECRET = process.env.ACCESS_SECRET;
+
 
 module.exports = async (req, res) => {
-    res.status(200).send({ data: null, message: 'ok' });
+    if (!req.headers.authorization) res.status(400).send({ data: null, message: 'invalid access token' });
+    else {
+        const authorization = req.headers.authorization;
+        const token = authorization.split(' ')[1];
+
+        try {
+            const { id } = req.body;
+            jwt.verify(token, ACCESS_SECRET);
+            await scrap.destroy({where: {id}})
+            
+            res.send({data: null, message: "scrap deleted"});
+            
+        } catch (err) {
+            res.status(400).send({data: err, message: 'invalid access token'});
+        }
+    }
 }
--- controllers/newsSearch/dbModify.js
+++ controllers/newsSearch/dbModify.js
@@ -1,5 +1,25 @@
-const { user } = require('../../models');
+const { category : categories } = require('../../models');
+const { scrap } = require('../../models');
+const jwt = require('jsonwebtoken');
+const ACCESS_SECRET = process.env.ACCESS_SECRET;
 
 module.exports = async (req, res) => {
-    res.status(200).send({ data: null, message: 'ok' });
+    if (!req.headers.authorization) res.status(400).send({ data: null, message: 'invalid access token' });
+    else {
+        const authorization = req.headers.authorization;
+        const token = authorization.split(' ')[1];
+
+        try {
+            const { id, category } = req.body;
+            jwt.verify(token, ACCESS_SECRET);
+            const categorydata = (await categories.findOne({where: {category}})).dataValues;
+            await scrap.update({categoryId: categorydata.id}, {where: {id}})
+
+            res.send({data: null, message: "category edited"});
+            
+        } catch (err) {
+            res.status(400).send({data: err, message: 'invalid access token'});
+        }
+
+    }
 }
