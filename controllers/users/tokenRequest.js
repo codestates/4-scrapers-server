59773533b0ff6e2d85686547dc49efd0f4c26f63
david419kr <david419kr@gmail.com>
2021-01-01 13:06:56 +0900

    36-google 로그인 / google 소셜로그인 구현하고 기존 컨트롤러들도 그에 맞춰 수정 완료

--- controllers/users/tokenRequest.js
+++ controllers/users/tokenRequest.js
@@ -1,5 +1,7 @@
 const { user } = require('../../models');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const qs = require('qs');
 const ACCESS_SECRET = process.env.ACCESS_SECRET;
 const REFRESH_SECRET = process.env.REFRESH_SECRET;
 
@@ -22,10 +24,17 @@ module.exports = async (req, res) => {
   else {
     const authorization = req.headers.authorization;
     const token = authorization.split(' ')[1];
-
+    let isGoogle = false;
+    if (token[4] === '.') isGoogle = true;
     try {
-      const data = cleanJWT(jwt.verify(token, ACCESS_SECRET));
-      const userInfo = await user.findOne({where: {email: data.email}})
+      let data;
+      if (isGoogle) {
+        data = (await axios.post("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + token, null)).data;
+      } else {
+        data = cleanJWT(jwt.verify(token, ACCESS_SECRET));
+      }
+
+      const userInfo = await user.findOne({ where: { email: data.email } })
       if (token === userInfo.dataValues.accessToken) {
         res.status(200).send({
           data,
@@ -39,40 +48,64 @@ module.exports = async (req, res) => {
         })
       }
     } catch (err) {
-      if (err.name === 'TokenExpiredError') {
-          const data = cleanJWT(jwt.decode(token, ACCESS_SECRET));
-          const userInfo = await user.findOne({where: {email: data.email}})
-          if (token !== userInfo.dataValues.accessToken) {
-            res.status(400).send({
-              data: null,
-              message: 'invalid access token'
-            });
-          } else if (!req.cookies.refreshToken) {
-              res.status(400).send({data: null, message: 'refresh token not provided'});
-          } else if (req.cookies.refreshToken !== userInfo.dataValues.refreshToken) {
-              res.status(400).send({data: null, message: "invalid refresh token"})
-          } else {
-              try {
-                jwt.verify(req.cookies.refreshToken, REFRESH_SECRET);
-                const accessToken = jwt.sign({
-                    id: data.id,
-                    email: data.email
-                }, ACCESS_SECRET, {
-                    expiresIn: '1 hours'
-                })
-                await user.update({accessToken}, {where: {email: data.email}})
-                res.status(200).send({
-                  data,
-                  accessToken,
-                  message: 'accessToken re-issued'
-                })
-              } catch (err) {
-                await user.update({refreshToken: null}, {where: {email: data.email}})
-                res.status(400)
-                .cookie('refreshToken', null, { httpOnly: true, sameSite: 'none' })
-                .send({data:null, message: "refresh token expired. please login again"})
-              }
+      if (isGoogle) {
+        if (!req.cookies.refreshToken) {
+          res.status(400).send({ data: null, message: 'refresh token not provided' });
+        } else {
+          try {
+            const queryStr = qs.stringify({
+              client_id: process.env.GOOGLE_CLIENT_ID,
+              client_secret: process.env.GOOGLE_CLIENT_SECRET,
+              refresh_token: req.cookies.refreshToken,
+              grant_type: "refresh_token"
+            })
+            const accessToken = (await axios.post('https://oauth2.googleapis.com/token?' + queryStr, null)).data.access_token;
+            const data = (await axios.post("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + accessToken, null)).data;
+            res.status(200).send({
+              data,
+              accessToken,
+              message: 'accessToken re-issued'
+            })
+          } catch (err) {
+            res.status(400)
+              .cookie('refreshToken', null, { httpOnly: true, sameSite: 'none' })
+              .send({ data: null, message: "refresh token expired. please login again" })
           }
+        }
+      } else if (err.name === 'TokenExpiredError') {
+        const data = cleanJWT(jwt.decode(token, ACCESS_SECRET));
+        const userInfo = await user.findOne({ where: { email: data.email } })
+        if (token !== userInfo.dataValues.accessToken) {
+          res.status(400).send({
+            data: null,
+            message: 'invalid access token'
+          });
+        } else if (!req.cookies.refreshToken) {
+          res.status(400).send({ data: null, message: 'refresh token not provided' });
+        } else if (req.cookies.refreshToken !== userInfo.dataValues.refreshToken) {
+          res.status(400).send({ data: null, message: "invalid refresh token" })
+        } else {
+          try {
+            jwt.verify(req.cookies.refreshToken, REFRESH_SECRET);
+            const accessToken = jwt.sign({
+              id: data.id,
+              email: data.email
+            }, ACCESS_SECRET, {
+              expiresIn: '1 hours'
+            })
+            await user.update({ accessToken }, { where: { email: data.email } })
+            res.status(200).send({
+              data,
+              accessToken,
+              message: 'accessToken re-issued'
+            })
+          } catch (err) {
+            await user.update({ refreshToken: null }, { where: { email: data.email } })
+            res.status(400)
+              .cookie('refreshToken', null, { httpOnly: true, sameSite: 'none' })
+              .send({ data: null, message: "refresh token expired. please login again" })
+          }
+        }
       }
       else res.status(400).send({ data: err, message: 'invalid access token' });
     }
